dfd8e69ef189b9fc444df05376e46a060bd9f1d4
jgarciagomez98 <jgarciagomez98@gmail.com>
2022-08-22 15:48:14 +0200

    Added navbar links

--- react-web/src/components/navbar/Navbar.jsx
+++ react-web/src/components/navbar/Navbar.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
+import "./navbar.css";
+
+const Menu = () => (
+  <Fragment>
+    <p>
+      <a href="#about">About</a>
+    </p>
+    <p>
+      <a href="#experience">Experience</a>
+    </p>
+    <p>
+      <a href="#work">Work</a>
+    </p>
+    <p>
+      <a href="#contact">Contact</a>
+    </p>
+  </Fragment>
+);
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar__links-container">
+        <Menu />
+      </div>
+      <button className="navbar__resume">Resume</button>
+    </div>
+  );
 }
 
 export default Navbar;
