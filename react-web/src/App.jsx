f882f33fc905df838bdb31b4b75022deb34f76c7
jgarciagomez98 <jgarciagomez98@gmail.com>
2022-08-22 12:41:48 +0200

    Added default navbar structure

--- react-web/src/App.jsx
+++ react-web/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Navbar } from "./components";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <div className="header__container">
+        <Navbar />
+      </div>
+    </div>
+  );
 }
 
 export default App;
